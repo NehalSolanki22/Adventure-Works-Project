794c36de6b57d8d9b0bcf359603ff8066fd1700e
NehalSolanki22 <89729940+NehalSolanki22@users.noreply.github.com>
2021-08-30 15:26:09 +0530

    Add files via upload

---
+++ Fact_InternetSales.sql
@@ -0,0 +1,14 @@
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  [SalesOrderNumber], 
+  [SalesAmount] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[FactInternetSales] 
+where 
+  LEFT(OrderDateKey, 4)>=(2014)-2 
+order by 
+  OrderDateKey
