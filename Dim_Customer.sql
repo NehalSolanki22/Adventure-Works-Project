9aba28489d79e82cbf0a2a5f538148949dfdd1ea
NehalSolanki22 <89729940+NehalSolanki22@users.noreply.github.com>
2021-08-30 15:25:13 +0530

    Add files via upload

---
+++ Dim_Customer.sql
@@ -0,0 +1,14 @@
+--Cleansed the Dim_Customer table
+SELECT 
+  c.[CustomerKey] as [CustomerKey], 
+  c.[FirstName] as [First Name], 
+  c.[LastName] as [Last Name], 
+  c.[FirstName] + '  ' + [LastName] as [Full Name], 
+  Case c.[Gender] when 'M' then 'Male' when 'F' then 'Female' end as [Gender], 
+  c.[DateFirstPurchase] as [DateFirstPurchase], 
+  g.[city] as [Customer City] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimCustomer] as c 
+  left join DimGeography as g on c.GeographyKey = g.GeographyKey 
+order by 
+  CustomerKey
